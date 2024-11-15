aa9fde7d69a229123864a2df9f537bda78222406
DaniGomAris <danielgomezaristizabal2020@gmail.com>
2024-11-14 23:22:00 -0500

    terminado-header

--- src/app/layout/pages/header/header.component.ts
+++ src/app/layout/pages/header/header.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { LibroService } from '../../../services/libro.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule, FormsModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  
+  libros: any[] = [];
+  tituloBusqueda: string = '';
+  resultadosVisible: boolean = false;  // Variable para controlar la visibilidad de los resultados de búsqueda
+
+  constructor(private libroService: LibroService) {}
+
+  buscarLibros(): void {
+    if (this.tituloBusqueda.trim()) {
+      this.libroService.buscarLibros(this.tituloBusqueda).subscribe(response => {
+        this.libros = response.docs;
+        this.resultadosVisible = this.libros.length > 0;  // Mostrar los resultados si existen
+      });
+    }
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: MouseEvent): void {
+    const searchBar = document.querySelector('.search-bar');
+    const resultados = document.querySelector('.resultados-libros');
+    
+    // Verifica si el clic fue fuera de la barra de búsqueda y los resultados
+    if (searchBar && !searchBar.contains(event.target as Node) && resultados && !resultados.contains(event.target as Node)) {
+      this.resultadosVisible = false;  // Oculta los resultados
+    }
+  }
 }
